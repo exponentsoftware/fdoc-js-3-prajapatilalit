be415f3f7f84a3b73f4521aa4c9d2cc7f8046318
prajapatilalit <prajapatilalit150992@gmail.com>
2021-09-22 19:51:47 +0530

    converted array to object

---
+++ Day3/arrayToObject.js
@@ -0,0 +1,13 @@
+const students = [
+  ["David", ["HTM", "CSS", "JS", "React"], [98, 85, 90, 95]],
+  ["John", ["HTM", "CSS", "JS", "React"], [85, 80, 85, 80]],
+];
+
+const arr1 = students.forEach((item1) => {
+  const objectArray = {
+    name: item1[0],
+    skills: item1[1],
+    scores: item1[2],
+  };
+  console.log(objectArray);
+});
